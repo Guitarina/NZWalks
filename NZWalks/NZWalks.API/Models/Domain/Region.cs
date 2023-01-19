1a35d03900d5c78e418a5c702e148857e2fb53b7
guitarina <singtomepls2020@gmail.com>
2023-01-19 22:50:32 +0100

    Added domain models

---
+++ NZWalks/NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,14 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public double Area { get; set; }
+        public double Long { get; set; }
+        public long Population { get; set; }
+
+        //Navigation property
+        public IEnumerable<Walk> Walks  { get; set; }
+    }
+}
---
+++ NZWalks/NZWalks.API/Models/Domain/WalkDifficulty.cs
@@ -0,0 +1,9 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class WalkDifficulty
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+
+    }
+}
